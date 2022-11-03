9a2cd8a42c5b938acc53304d95ecce100ce82d49
Ramwh <vasilkovd_67@mail.ru>
2022-11-03 21:19:38 +0400

    Добавлен решение первой задачи

---
+++ DZ6.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            void printArray(int[] array)
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] createArray(int size)
+            {
+                int[] array = new int[size];
+                for (int i=0; i<size; i++)
+                {
+                    Console.Write($"Введите {i+1} число: ");
+                    array[i] = Convert.ToInt32(Console.ReadLine()); 
+                }
+                return array;
+            }
+
+            int countPositiveNumbers(int[] array)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > 0) count ++;
+                }
+                return count;
+
+            }
+            
+
+
+            Console.Write("Сколько чисел вы собираетесь ввести? ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = createArray(arraySize);
+            printArray(numbers);
+            Console.WriteLine($"Количество положительных чисел: {countPositiveNumbers(numbers)}");
+        }
+    }
+}
